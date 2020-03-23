5bf913d4d1ca8b54e0fe02bd5e20ed8c6665dbd7
Michal <michalogloza97@gmail.com>
2020-03-23 17:43:55 +0100

    Prepared tax calculation interface and its implementation template.

---
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/RegularTaxCalculation.java
@@ -0,0 +1,10 @@
+package pl.com.bottega.ecommerce.sales.domain.invoicing;
+
+public class RegularTaxCalculation implements TaxCalculationType
+{
+    @Override
+    public void calculateTax(RequestItem requestItem)
+    {
+
+    }
+}
